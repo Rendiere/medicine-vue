5ef702bf072d2fe51fba7c140822b8bece5f8d8f
renierbotha <r.botha91@gmail.com>
2018-07-16 13:26:05 +0200

    First commit - adds basic site with minimal functionality

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app light>
+
+    <v-navigation-drawer v-model="sideNav" fixed>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>music_note</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Mixes</v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>group_add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Sign Up</v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>graphic_eq</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>About</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar flat @click.stop="sideNav = false">
+      <v-toolbar-side-icon
+        class="hidden-sm-and-up"
+        @click.stop="sideNav = !sideNav">
+      </v-toolbar-side-icon>
+
+
+      <v-toolbar-title>Medicine Monthly</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>music_note</v-icon>
+          Mixes
+        </v-btn>
+
+        <v-btn flat>
+          <v-icon left>group_add</v-icon>
+          Sign Up
+        </v-btn>
+
+        <v-btn flat>
+          <v-icon left>graphic_eq</v-icon>
+          About
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    },
+    name: 'App'
+  }
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-content>
+    <section>
+      <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" height="600">
+        <v-layout
+          column
+          align-center
+          justify-center
+          class="white--text">
+
+          <img src="assets/logo.png" alt="logo" height="300">
+          <h1 class="white--text mb-2 display-1 text-xs-center">Medicine</h1>
+          <div class="subheading mb-3 text-xs-center">Monthly curated playlists</div>
+
+        </v-layout>
+      </v-parallax>
+    </section>
+
+    <section>
+      <v-layout
+        mt-5
+        column
+        wrap
+        align-center
+        justify-center>
+
+        <!--Mixes heading-->
+        <v-flex xs-12>
+          <div class="text-xs-center">
+            <h2 class="headline">Our Mixes</h2>
+            <span class="subheading">A subheading...</span>
+          </div>
+        </v-flex>
+
+        <!--Mixes list-->
+        <v-flex xs-12>
+          <v-container grid-list-xl>
+            <v-layout row wrap align-center justify-center>
+              <v-flex  xs12 s6 md4 v-for="mix in mixes" :key="mix.title">
+                <v-card flat hover="true" ripple="true">
+                  <v-card-title class="justify-center card--title">{{mix.title}}</v-card-title>
+                  <v-card-media :src="mix.cover" height="200" width="200"></v-card-media>
+                </v-card>
+              </v-flex>
+            </v-layout>
+
+            <!--TODO: Load more when scrolled to botom-->
+          </v-container>
+        </v-flex>
+      </v-layout>
+
+    </section>
+  </v-content>
+
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        mixes: [
+          {
+            cover: '/static/images/FEB18.png',
+            title: 'FEB18'
+          },
+          {
+            cover: '/static/images/MAR18.png',
+            title: 'MAR18'
+          },
+          {
+            cover: '/static/images/APR18.png',
+            title: 'APR18'
+          },
+          {
+            cover: '/static/images/MAY18.png',
+            title: 'MAY18'
+          },
+          {
+            cover: '/static/images/JUNE18.png',
+            title: 'JUN18'
+          },
+          {
+            cover: '/static/images/JUL18.png',
+            title: 'JUL18'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .card--title {
+    font-weight: lighter;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
